de4a4f92ac483a7eea7f583e84d483b79710d451
HeyItsFinnVR <finn1011@live.com>
2023-07-24 21:52:32 +0100

    Api calls and Map working... mostly

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-
-  </header>
+  <Map :coords="coords"/>
 </template>
+
+<script>
+
+  import Map from "./components/map.vue"
+
+  const cityName = "london"
+  const apiKey = ""   // Get from API Ninjas            // Figure out how to import a var from a .env file ASAP!
+  const request = {
+    method: "GET",
+    headers: { "X-Api-Key": apiKey },
+  }
+  // Commented out so to not use unnessasary api calls when developing
+  //const response = await fetch("https://api.api-ninjas.com/v1/city?name=" + cityName, request)
+  const data = {name: "london", population: 10979000, latitude: 51.5072, longitude: -0.1275}  //await response.json()
+
+  export default {
+    name: "App",
+    components: {
+      Map
+    },
+    data() {
+      return {
+        coords: [data.latitude, data.longitude]
+      }
+    }
+  }
+</script>
---
+++ src/components/Map.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="map container">
+    <div id="mapId" class="map"></div>
+  </div>
+</template>
+  
+<script>
+  import { onMounted } from "vue"
+  import leaflet from "leaflet"
+  export default {
+    name: "Map",
+    props: {
+      coords: Array
+    },
+    setup(props) {
+      let myMap
+      onMounted(() => {
+        myMap = leaflet.map("mapId").setView(props["coords"], 13)
+  
+        leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', 
+        {
+          maxZoom: 20,
+          attribution: '© OpenStreetMap'
+        }).addTo(myMap);
+      })
+    }
+  }
+</script>
+
+<style>
+  .map.container {
+    background-color: hsl(210, 50%, 2%);
+    width: 75%;
+    height: auto;
+    aspect-ratio: 2/1;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+  .map {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+  }
+</style>
